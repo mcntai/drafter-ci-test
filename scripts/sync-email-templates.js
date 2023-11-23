04394e717bf7b8846705c59c0dfff76a274ded7e
Maxim Sheiko <68130217+mcntai@users.noreply.github.com>
2023-11-23 16:41:50 +0200

    something (#1) (#9)

--- scripts/sync-email-templates.js
+++ scripts/sync-email-templates.js
@@ -4,18 +4,24 @@
 
   const filePath = path.join(__dirname, 'data.json')
 
-  const rawData = await fs.readFile(filePath)
-  const data = JSON.parse(rawData)
+  try {
+    const rawData = await fs.readFile(filePath)
+    const data = JSON.parse(rawData)
 
-  data.push({ name: 'name', id: 'id' })
+    data.push({ name: 'name', id: 'id' })
 
-  const updatedData = JSON.stringify(data, null, 2)
+    const updatedData = JSON.stringify(data, null, 2)
 
-  await fs.writeFile(filePath, updatedData)
+    await fs.writeFile(filePath, updatedData)
 
-  console.log(JSON.stringify(data, null, 2))
+    console.log(JSON.stringify(data, null, 2))
 
-  console.log({ syncResult: Boolean(data.length) })
+    console.log(JSON.stringify({ syncResult: Boolean(data.length) }))
 
-  return Boolean(data.length).toString()
+    // Return the result in a format that can be captured by GitHub Actions
+    process.exitCode = 0 // Indicate success
+  } catch (error) {
+    console.error(error.message)
+    process.exitCode = 1 // Indicate failure
+  }
 })()
