e5b2e03b5eb9bc35e8f72e513bbf6f7e19871fee
Maxim Sheiko <maksym.sheiko@backendlessmail.com>
2023-11-23 17:07:48 +0200

    13

--- scripts/sync-email-templates.js
+++ scripts/sync-email-templates.js
@@ -4,19 +4,14 @@
 
   const filePath = path.join(__dirname, 'data.json')
 
-  try {
-    const rawData = await fs.readFile(filePath)
-    const data = JSON.parse(rawData)
+  const rawData = await fs.readFile(filePath)
+  const data = JSON.parse(rawData)
 
-    data.push({ name: 'name', id: 'id' })
+  data.push({ name: 'name', id: 'id' })
 
-    const updatedData = JSON.stringify(data, null, 2)
+  const updatedData = JSON.stringify(data, null, 2)
 
-    await fs.writeFile(filePath, updatedData)
+  await fs.writeFile(filePath, updatedData)
 
-    return Boolean(data.length)
-  } catch (error) {
-    console.error(error.message)
-    process.exitCode = 1 // Indicate failure
-  }
+  console.log(Boolean(data.length))
 })()
