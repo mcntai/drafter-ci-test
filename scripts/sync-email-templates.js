9b611e39a1249cbd7a6b72f9a57e66d2b3ee34be
Maxim Sheiko <maksym.sheiko@backendlessmail.com>
2023-11-24 18:53:32 +0200

    template-changed

--- scripts/sync-email-templates.js
+++ scripts/sync-email-templates.js
@@ -1,6 +1,10 @@
 (async () => {
   const path = require('path')
   const fs = require('fs').promises
+  const { promisify } = require('util')
+  const { exec } = require('child_process')
+
+  const execAsync = promisify(exec)
 
   const filePath = path.join(__dirname, 'templates-meta-data.json')
 
@@ -14,6 +18,10 @@
   const updatedData = JSON.stringify(data, null, 2)
 
   await fs.writeFile(filePath, updatedData)
+
+  await execAsync('git add ./templates-meta-data.json')
+  await execAsync('git commit -m "update email templates meta data [ci skip]"')
+  await execAsync('git push')
 
   return Boolean(data.length)
 })()
