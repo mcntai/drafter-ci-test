13f8f210e21e2559a5280fdb0f6b006e04892471
Maxim Sheiko <maksym.sheiko@backendlessmail.com>
2023-11-27 11:21:02 +0200

    template-changed

--- scripts/sync-email-templates.js
+++ scripts/sync-email-templates.js
@@ -1,23 +1,104 @@
+import { promisify } from 'util'
+import { exec } from 'child_process'
+
 (async () => {
   const path = require('path')
   const fs = require('fs').promises
+  const { promisify } = require('util')
+  const { exec } = require('child_process')
+
+  const execAsync = promisify(exec)
+
+  const isObject = obj => obj === Object(obj)
+
+  const isDate = date => date instanceof Date
+
+  const clone = (obj, { preserveTypes = true } = {}) => {
+    if (Array.isArray(obj)) {
+      return obj.concat()
+    }
+
+    if (isObject(obj)) {
+      const clone = preserveTypes ? Object.create(Object.getPrototypeOf(obj)) : {}
+
+      return Object.assign(clone, obj)
+    }
+
+    return obj
+  }
+
+  const cloneDeep = (obj, options) => {
+    if (Array.isArray(obj)) {
+      return obj.map(item => cloneDeep(item, options))
+    }
+
+    if (obj instanceof Date) {
+      return new Date(obj)
+    }
+
+    if (isObject(obj) && !isDate(obj)) {
+      const result = clone(obj, options)
+
+      Object.keys(obj).forEach(key => {
+        result[key] = cloneDeep(obj[key], options)
+      })
+
+      return result
+    }
+
+    return obj
+  }
+
+  const equals = (a, b) => a === b || JSON.stringify(a) === JSON.stringify(b)
+
+  const compare = (a, b, attributes) => {
+    if (!attributes) {
+      attributes = Object.keys(a)
+    }
+
+    return attributes.filter(attr => !equals(a[attr], b[attr]))
+  }
+
+  const commitUpdatedTemplatesMetaData = async () => {
+    const userEmail = `-c user.email="${process.env.GITHUB_ACTOR}@users.noreply.github.com"`
+    const userName = `-c user.name="${process.env.GITHUB_ACTOR}"`
+
+    await execAsync('git add scripts/sync/email-templates/templates-meta-data.json')
+    await execAsync(`git ${userEmail} ${userName} commit -m "add updated templates meta data [ci skip]"`)
+    await execAsync('git push')
+  }
+
+  const shouldCommitChanges = templatesDataBeforeChanges => {
+    console.log({ templatesDataBeforeChanges })
+
+    const changes = compare(templatesDataBeforeChanges, require('./templates-meta-data.json'))
+
+    return changes.length
+  }
 
   const filePath = path.join(__dirname, 'templates-meta-data.json')
 
-  console.log(Boolean(filePath))
-  console.log(Boolean(filePath))
+  const templatesMetaDataMap = require('./templates-meta-data.json')
 
-  const rawData = await fs.readFile(filePath)
-  const data = JSON.parse(rawData)
+  const templatesDataBeforeChanges = cloneDeep(templatesMetaDataMap)
 
-  data.push({ name: 'name', id: 'id' })
+  const template = templatesMetaDataMap.RESET_PASSWORD
+
+  if (template) {
+    template.name = 'updated'
+  } else {
+    templatesMetaDataMap[name] = { name: 'name', id: 'id' }
+  }
 
   const updatedData = JSON.stringify(data, null, 2)
 
   await fs.writeFile(filePath, updatedData)
 
-  console.log(Boolean(data.length))
-  console.log(Boolean(data.length))
+  if (shouldCommitChanges(templatesDataBeforeChanges)) {
+    console.log('should commit changes')
+
+    await commitUpdatedTemplatesMetaData()
+  }
 
   return Boolean(data.length)
 })()
